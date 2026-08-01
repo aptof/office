4a84649a9bcf70be73c7726c7a1fdc4e878d6245
Tushar Bakshi <developer.aptof@outlook.com>
2026-08-01 10:58:04 +0530

    Added all better-auth, drizzle and shadcn

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@/lib/db';
+import { nextCookies } from 'better-auth/next-js';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite', // or "mysql", "pg"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
